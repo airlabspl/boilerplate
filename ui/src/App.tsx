f42048f87f6315e79b7037854c50902e10d5c237
Krzysztof <krzysztof@engineer.com>
2026-02-24 12:03:25 +0100

    feat: add shadcn

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,5 +1,16 @@
+import { BrowserRouter, Route, Routes } from "react-router";
+import Home from "./page/home";
+import Dashboard from "./page/dashboard";
+
 function App() {
-  return <>Hello, world</>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="dashboard" element={<Dashboard />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App;
